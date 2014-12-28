ea3ece9b2a792cf634aff93b1b4a951f6218ab9f
Sean Kell <sean.michael.kell@gmail.com>
2014-12-27 21:05:26 -0500

    Add support for injecting the port and client dir.

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 const VERSION = "0.1.0"
 
+var clientDir string
+
+func init() {
+	flag.StringVar(&clientDir, "client", "./client", "the directory where the client data is stored")
+}
+
 func main() {
 	log.Printf("resolutionizerd %s starting...", VERSION)
 
-	log.Fatalln(http.ListenAndServe(":8080", http.FileServer(http.Dir("./client"))))
+	log.Fatalln(http.ListenAndServe(":"+os.Getenv("PORT"), http.FileServer(http.Dir(clientDir))))
 }
