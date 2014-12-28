fd707a94d90dff42cf8fd61d88e2dc2e31231eec
Sean Kell <sean.michael.kell@gmail.com>
2014-12-28 09:53:03 -0500

    Remove temporary logging.

--- main.go
+++ main.go
@@ -20,15 +20,16 @@ func main() {
 	flag.Parse()
 	fmt.Printf("resolutionizerd %s starting...\n", VERSION)
 	fmt.Printf("listening on port %s\n", os.Getenv("PORT"))
-	fmt.Printf("client environment variable setting: %s\n", os.Getenv("CLIENT"))
-	fmt.Printf("client root: %s\n", clientDir)
 
 	if clientDir == "" {
 		clientDir = os.Getenv("CLIENT")
 	}
 
+	fmt.Printf("client root: %s\n", clientDir)
+
 	if _, err := os.Stat(clientDir); err != nil {
 		fmt.Println(err)
+		os.Exit(1)
 	}
 
 	http.Handle("/", http.FileServer(http.Dir(clientDir)))
